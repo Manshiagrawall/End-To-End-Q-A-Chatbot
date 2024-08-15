a7792748c75b6e625346d6be13eb666e118a3b5f
Manshi Agrawal <124449849+Manshiagrawall@users.noreply.github.com>
2024-08-15 22:56:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 import speech_recognition as sr
+import pyaudio
+
 
 # Define custom CSS
 st.markdown("""
